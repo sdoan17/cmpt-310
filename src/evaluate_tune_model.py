24c942eb2f103df063a75ec11dd9a625b3166f11
Simon D <doannhandv1230@gmail.com>
2025-08-09 11:39:53 -0700

    added markdown files

--- src/evaluate_tune_model.py
+++ src/evaluate_tune_model.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, confusion_matrix
 
 # Load test set
 X_test, y_test = joblib.load('../models/test_set.pkl')
@@ -29,5 +29,14 @@ with open(report_path, 'w') as f:
         f.write(f"=== {name} ===\n")
         f.write(report)
         f.write("\n\n")
+
+        cm = confusion_matrix(y_test, y_pred)
+        plt.figure(figsize=(6,5))
+        sns.heatmap(cm.T, annot = True, fmt='d', cmap = "Blues",
+                    xticklabels=['Actual_Dropout', 'Actual_No_Risk'],
+                    yticklabels=['Predicted_Dropout', 'Predicted_No_Risk'])
+        plt.title(f'{name.title()} Tuned Confusion Matrix')
+        plt.savefig(f'../reports/{name}_tuned_confusion_matrix.png', bbox_inches = 'tight')
+        plt.close()  
 
 print(f"Combined report saved to {report_path}")
